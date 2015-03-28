4b6fccd9ce592eb72c29711bb4d5557e45cc4549
yadongshao <348829719@qq.com>
2015-03-28 13:08:23 +0800

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,46 @@
+/*简易队列实现 模板类只支持内部数据类型*/
+#pragma once
+#ifndef _QUEUE_H
+#define _QUEUE_H
+template<class T>
+class Queue{
+public:
+	Queue(int capacity=10):start(0),rear(0){
+		this->capacity=capacity;
+		queue=new T[capacity];
+	}
+	~Queue(){
+		delete[] queue;
+		queue=NULL;
+	}
+	bool isEmpty(){
+		return rear==start?true:false;
+	}
+	bool isFull(){
+		return (rear+1)%capacity==start%capacity?true:false;
+	}
+	void push(const T& element){
+		if(isFull()) throw "the queue is full!";
+		else{
+			queue[rear%capacity]=element;
+			rear++;
+		}
+	}
+	T pop(){
+		if(isEmpty()) throw "the queue is empty!";
+		else{
+			T temp=queue[start%capacity];
+			start++;
+			return 	temp;
+		}
+	}
+	int getNum(){
+		return (rear-start);
+	}
+private:
+	int capacity;
+	T*  queue;
+	int start;
+	int rear;
+};
+#endif
